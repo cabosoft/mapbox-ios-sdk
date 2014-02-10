c78d1e86332c449d686af58c83ad1f7a0f5bd8a6
Jim Boyd <jimb@cabosoft.com>
2014-02-10 16:54:19 -0700

    Allow tile sources to be blended with different alpha elves and blend modes

--- MapView/Map/RMMBTilesSource.m
+++ MapView/Map/RMMBTilesSource.m
@@ -45,7 +45,7 @@
     NSString *_uniqueTilecacheKey;
 }
 
-@synthesize cacheable = _cacheable, opaque = _opaque;
+@synthesize cacheable = _cacheable, opaque = _opaque, alpha = _alpha, blendMode = _blendMode;
 
 - (id)initWithTileSetResource:(NSString *)name
 {
@@ -80,6 +80,8 @@
 
     self.cacheable = NO;
     self.opaque = YES;
+    self.alpha = 1.0;
+    self.blendMode = kCGBlendModeNormal;
 
 	return self;
 }
